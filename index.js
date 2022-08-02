1c0435a95de8084d49587420db1e6f32a030baf7
Iskander Siranov <iskander.siranov@gmail.com>
2022-08-02 16:26:58 +0600

    commands

--- index.js
+++ index.js
@@ -10,5 +10,21 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	} else if (commandName === 'text') {
+		await interaction.reply('Some text');
+	}
+});
+
 // Login to Discord with your client's token
 client.login(token);
